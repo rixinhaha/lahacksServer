3cf4852c23cde37f9eede34a16b3ce141dc910b9
Charles Liu <liuchangyang112@gmail.com>
2020-03-29 12:30:40 +0800

    Partially completed database implementation for index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http  = require('http');
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./user.js');
 
 const PORT = 5000;
 const router =  require('./router');
@@ -10,20 +9,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const db = require('./db-utils.js');
+
 io.on('connection', (socket)=>{
     console.log("We have a new connection!!!");
-    socket.on('join', ({name,room}, callback)=>{
-        const {error, user} = addUser({id: socket.id, name, room});
-        if(error) return callback(error);
-        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`});
-        socket.join (user.room);
-        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
-        callback();
+    socket.on('join', async ({name,room}, callback)=>{
+        const res = await db.createRoom(room);
+        if(res.error){
+            return callback(error);
+        }else{
+            res = await db.addUserToRoom({user: name, room});
+            if(res.error){
+                return callback(error)
+            }else{
+                socket.emit('message', {user: 'admin', text: `${name}, welcome to the room ${room}`});
+                socket.broadcast.to(room).emit('message', {user: 'admin', text: `${name}, has joined!`});
+                socket.join (room);
+                io.to(room).emit('roomData', {room: room, users: getUsersInRoom(room)})
+                callback();
+            }
+        }
     });
-    socket.on('sendMessage',(message, callback)=>{
-        const user =  getUser(socket.id);
-        console.log(message)
+    socket.on('sendMessage', async (message, callback)=>{
+        const res =  db.getUser(socket.id);
+        //console.log(message)
         io.to(user.room).emit('message', {user: user.name, text: message});
         io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
         callback()
@@ -38,6 +47,7 @@ io.on('connection', (socket)=>{
 })
 
 app.use(router);
+app.use(express.urlencoded());
 
 server.listen(PORT, ()=>{console.log(`Server has started on ${PORT}`)});
 
