fc2cecf4bb32ca2c2306fef474d80284c74725d5
Charles Liu <liuchangyang112@gmail.com>
2020-03-29 10:53:08 +0800

    Added get messages endpoint

--- router.js
+++ router.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router  = express.Router();
+const db = require('./db-utils.js')
 
 router.get('/', (req, res)=>{
     res.send('server is up and running');
+});
+
+router.get('/rooms/:roomname/messages', async (req, res) => {
+    const messages = await db.getMessagesInRoom({
+        room: req.params.roomname,
+        numMessages: req.query.num,
+        startId: req.query.start_id
+    });
+    if(messages.error) {
+        console.log(messages.error);
+        res.sendStatus('404');
+    }else{
+        messages.last_id = res.data[res.data.length - 1]._id;
+       res.json(messages); 
+    }
 });
 
 module.exports = router
