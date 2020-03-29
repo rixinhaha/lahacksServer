2556c34f6f37923fbc4444d95c70f12ccbe975b4
Shaodian Wang <diandianws@gmail.com>
2020-03-29 15:32:45 +0800

    socket io integration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http  = require('http');
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./user.js');
+const {addUser, removeUser, getUserAndRoom} = require('./user.js');
+const db = require('./db-utils')
 
 const PORT = 5000;
 const router =  require('./router');
@@ -10,28 +11,78 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+
 io.on('connection', (socket)=>{
     console.log("We have a new connection!!!");
-    socket.on('join', ({name,room}, callback)=>{
-        const {error, user} = addUser({id: socket.id, name, room});
-        if(error) return callback(error);
-        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`});
-        socket.join (user.room);
-        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
-        callback();
+    socket.on('join', async ({name,room}, callback)=>{
+        // const {error, user} = addUser({id: socket.id, name, room});
+        let resRoom;
+        let resUser;
+        let resAddUser;
+        try{
+            resRoom = await db.createRoom(room);
+        } catch (err){
+            return {err:err};
+        }
+        console.log(resRoom);
+
+        try{
+            resUser = await db.createUser(name);
+        }
+        catch (err){
+            return {err:err};
+        }     
+        // console.log(resUser);
+
+        try{
+            resAddUser = await db.addUserToRoom({user: name, room:room});
+        }
+        catch (err){
+            return {err:err};
+        }
+
+        addUser({
+            user: name,
+            id: socket.id,
+            room: room         
+        });
+
+        socket.emit('message', {user: 'admin', text: `${name}, welcome to the room ${room}`});
+        socket.broadcast.to(room).emit('message', {user: 'admin', text: `${name}, has joined!`});
+        socket.join (room);
+
+        // callback();
     });
-    socket.on('sendMessage',(message, callback)=>{
-        const user =  getUser(socket.id);
-        console.log(message)
-        io.to(user.room).emit('message', {user: user.name, text: message});
-        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+    socket.on('sendMessage',async(message, callback)=>{
+        const info =  getUserAndRoom(socket.id);
+        console.log(info);
+        let res;
+
+        try{
+            res = await db.addMessage({user: info.user, message: message, room: info.room});
+        }
+        catch(err){
+            return {err:err};
+        } 
+
+        io.to(info.room).emit('message', {user: info.user, text: message});
+
         callback()
     });
-    socket.on('disconnect', ()=>{
-        const user = removeUser(socket.id)
-        if(user){
-            io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left`});
+    socket.on('disconnect', async()=>{
+        const info =  getUserAndRoom(socket.id);
+
+        let res;
+        try{
+            res = await db.removeUserFromRoom({user: info.user, room: info.room});
+            console.log(res);
+            
+        }catch (err){
+            return {err:err};
+        }        
+        removeUser(socket.id);
+        if(info){
+            io.to(info.room).emit('message', {user: 'admin', text: `${info.user} has left`});
         }
         console.log('User had left!!')
     });
